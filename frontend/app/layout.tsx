657c55d0ffbaad27d0920b74df6ca9a7ae65f34f
kshitij-hash <kjangra1557@gmail.com>
2025-10-28 03:32:16 +0530

    added analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { APP_CONFIG } from "@/config/constants";
+import { Analytics } from "@vercel/analytics/next"
 
 const openSans = Open_Sans({
   variable: "--font-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className={`${openSans.variable} font-sans antialiased`}>
         <Providers>
           {children}
+          <Analytics />
         </Providers>
       </body>
     </html>
